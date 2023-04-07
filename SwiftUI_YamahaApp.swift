4caff3c0e35180610430b43d4ffe398b1b792048
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-04-07 17:29:13 -0500

    Sprint 1

---
+++ SwiftUI_YamahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_YamahaApp.swift
+//  SwiftUI-Yamaha
+//
+//  Created by Josh Zhuchenko on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_YamahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
